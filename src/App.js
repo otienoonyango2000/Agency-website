3fed1a81e9399e040818a4967397bd510bed4243
george <otienoonyango2000@gmail.com>
2023-10-22 01:38:04 +0300

    create service section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './component/Home';
 
 import Navbar from './component/Navbar';
+import Services from './component/Services';
 
 function App() {
   return (
     <div>
     <Navbar/>
     <Home/>
+    <Services/>
     </div>
   );
 }
---
+++ src/component/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
+        <div className='text-center my-8'>
+            <h2 className='text-4xl text-neutralDGrey text-semibold mb-2'>Our Clients</h2>
+            <p className='text-neutral-Grey'>We have been working with some fortune 500+ clients</p>
+        </div>
+    </div>
+  )
+}
+
+export default Services
