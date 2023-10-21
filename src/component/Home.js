b647d58d5f2d62c4623f92ac7df5962b3b04ec5e
george <otienoonyango2000@gmail.com>
2023-10-22 01:15:26 +0300

    carousel create

--- src/component/Home.js
+++ src/component/Home.js
@@ -19,6 +19,19 @@ const Home = () => {
                         transtion-all duration-300 hover:-translate-y-4'>Register</button>
                     </div>
                 </div>
+
+                <div className="my-28 md:my-28 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+                    <div>
+                        <img className='w-20' src={banner} alt='img'/>
+                    </div>
+                    {/* hero text */}
+                    <div className='md:w-1/2'>
+                        <h1 className='text-4xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>Learn and earn <span className='text-brandPrimary leading-snug'>in 4 months</span></h1>
+                        <p className='text neutralGray text-base mb-8'>Where to grow your business as a photographer : sight or social media?</p>
+                        <button className='px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neuralDGrey
+                        transtion-all duration-300 hover:-translate-y-4'>Register</button>
+                    </div>
+                </div>
               
             </Carousel>
         </div>
